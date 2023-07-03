38d1c0a0c62d7255e6ea62129307aac1d745e892
Igorippon <botvunaigor@gmail.com>
2023-07-03 15:08:14 +0300

    add.task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,34 @@
-import Player from '@vimeo/player';
-console.log(Player);
+import Player from '@vimeo/player'; 
+
+const iframe = document.querySelector('#vimeo-player')
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+const onPlay = function ({seconds}) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+    console.log(seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000)); 
+
+const LS_Play = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(LS_Play).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+
+
+    
