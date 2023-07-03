8a29cd696f64a0b0b38890fd672ce7f29e95b88a
Igorippon <botvunaigor@gmail.com>
2023-07-04 01:43:50 +0300

    fix.task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,28 @@
 const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
-const input = document.querySelector('#email');
 const STORAGE_KEY = "feedback-form-state"
-const data = {};
+let data = {};
 
 form.addEventListener('input', throttle(handlerInput, 500));
 function handlerInput(evt) {
     data[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-   }
+    
+}
+   
+data = getData();
+
+for (let key in data) {
+    form.elements[key].value = data[key];
+}
 
 form.addEventListener('submit', handlerSubmit);
 function handlerSubmit(evt) {
     evt.preventDefault();
-    
     const { email, message } = evt.target.elements;
     const emailVel = email.value.trim();
     const messageVel = message.value.trim();
-    const data = createdObj(emailVel, messageVel);
+     data = createdObj(emailVel, messageVel);
     console.log(data);
    localStorage.removeItem(STORAGE_KEY)
     evt.target.reset();
@@ -32,8 +37,8 @@ function createdObj(email, message) {
 function getData() {
     try{
     const data = localStorage.getItem(STORAGE_KEY);
-    if (data) {
-        return JSON.parse(data)
+        if (data) {
+           return JSON.parse(data)
             }
         return {};
     } catch(err) {
